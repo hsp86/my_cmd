a88834660b8b79a088026cd2e3253fd1ab4518f9
hsp86 <husipeng86@live.com>
2016-10-15 15:22:58 +0800

    添加命令到文件和从文件读取命令内容

--- config.py
+++ config.py
@@ -3,12 +3,17 @@
 # 输入命令显示提示字符
 prompt_header = "hsp>"
 
+# 指定存储命令的文件
+cmd_file = "cmd.txt"
+
 # 指定结束输入命令的字符
 # 如使用windows结束符"\r\n"所以可指定为"\r"
 cmd_end = "\r"
 
-# 指定存储命令的文件
-cmd_file = "cmd.txt"
-
-
+# 指定系统目录分割符
+separate_char = "\\"
+# 后缀分隔符；命令名默认为去除后缀的文件名
+suffix_char = "."
+# 文件中存储的命令格式为：命令名+以下字符+命令内容
+div_char = ";"
 
--- my_cmd.py
+++ my_cmd.py
@@ -3,10 +3,20 @@
 import os,sys,re
 import config
 
-my_cmd = ["husipeng","hsp86"]
+cmd_list = [] # 存放命令名及其内容
+cmd_name_list = ["husipeng","hsp86"] # 只存放命令名
 def init():
-    '''初始化命令，从文件中读取并存入各变量'''
-    pass
+    '''初始化命令，从文件中读取命令及其内容'''
+    f = file(config.cmd_file,'r') # 打开存储命令的文件
+    line = f.readline()[:-1] # 读取一行，且去除行末的"\n"
+    print line
+    while len(line) > 0:
+        cmd_detail = line.split(config.div_char)
+        cmd_list.append([cmd_detail[0],cmd_detail[1]]) # 存入命令名及其内容，这里说明若要在文件中添加注释可用config.div_char，且不会被计入cmd_list
+        cmd_name_list.append(cmd_detail[0]) # 将命令名存入
+        line = f.readline()[:-1] # 读取一行，且去除行末的"\n"
+    print cmd_list
+    f.close()
 
 def search_start(src_list,search_str):
     '''查找src_list中以search_str开始的项，返回找到的个数和list'''
@@ -67,7 +77,7 @@ def del_display(st):
 # 用于存储运行过的命令
 cmd_history = ["husipeng","hsp86"]
 def get_cmd_input():
-    '''本函数完成接收用户的输入并返回输入的字符，支持tab搜索已有命令，tab循环显示搜索结果'''
+    '''本函数完成接收用户的输入并返回输入的字符，支持tab搜索已有命令，tab循环显示搜索结果，上下键浏览历史命令'''
     from _Getch import _Getch
     getch = _Getch()
     cmd = ""
@@ -79,7 +89,7 @@ def get_cmd_input():
     while ch != config.cmd_end: # enter键完成命令输入结束符可在config中配置
         if ch == "\t": # tab键
             if display_num == 0: # 若没搜索过就重新搜索
-                search_res_num,search_res = fuzzy_search(my_cmd,cmd) # 模糊搜索cmd的项
+                search_res_num,search_res = fuzzy_search(cmd_name_list,cmd) # 模糊搜索cmd的项
             # 显示搜索到的第search_res_num个，并设置到cmd
             if search_res_num > 0: # 搜索到的结果大于0才重新显示，否则不修改cmd和显示
                 if search_res_num <= display_num:
@@ -130,16 +140,33 @@ def get_cmd_input():
     return cmd
 
 def disp_msg(msg):
+    '''显示信息，输入Enter后退出本函数'''
     print msg
     print u"\t输入Enter继续"
     raw_input()
 
+def add_cmd(add_list):
+    f = file(config.cmd_file,'a') # 打开存储命令的文件
+    for x in add_list:
+        last_separate = x.rfind(config.separate_char) # 文件命令开始的位置，最后一个目录分隔符后开始
+        last_suffix = x.rfind(config.suffix_char) # 用于文件名后缀
+        if last_separate < last_suffix: # 这种情况说明命令文件名才对，否则不添加
+            f.write(x[last_separate+1:last_suffix-1] + config.div_char + x + "\n") # 将命令名和命令内容用config.div_char分开后写入文件中
+            # disp_msg(u"添加命令成功：" + x[last_separate+1:last_suffix-1])
+        # else:
+            # disp_msg(u"添加命令失败：" + x)
+    f.close()
+
 def mycmd_execute(cmd):
     '''当做自定义命令执行'''
-    fid = os.popen(cmd)
-    print fid.read()
-    statu_cod = fid.close()
-    if statu_cod != None:
+    # fid = os.popen(cmd)
+    # print fid.read()
+    # statu_cod = fid.close()
+    for x in cmd_list:
+        if x[0] == cmd: # 依次顺序查找命令，直到找到后执行并退出执行
+            fid = os.popen(x[1])
+            break
+    if 0 != None:
         print u'命令执行失败！'
         return -1
     else:
@@ -156,16 +183,15 @@ def syscmd_execute(cmd):
     else:
         return 0
 
-
 if __name__ == "__main__":
-    arg_len = len(sys.argv[0])
-    if arg_len > 1: # 当输入参数大于1时（如将文件拖到本执行文件执行），就为添加命令到文件
-        pass
-    else: # 当输入参数为1时表示，直接运行，进行输入命令处理
+    argv_len = len(sys.argv)
+    if argv_len > 1: # 当输入参数个数大于1时（如将文件拖到本执行文件执行），就为添加命令到文件
+        add_cmd(sys.argv[1:]) # 添加命令到文件
+    else: # 当输入参数量为1时表示，直接运行，进行输入命令处理
+        init() # 初始化命令，从文件中读取命令及其内容
         cmd = get_cmd_input()
-        while cmd != 'quit':
+        while cmd != 'quit': # 输入quit时退出
             if mycmd_execute(cmd) != 0: # 如果自定义命令执行失败，即cmd不在自定命令中，则当做系统命令来执行
                 syscmd_execute(cmd)
             cmd = get_cmd_input() # 执行后继续输入
         disp_msg(u"退出？")
-
